cfd90517e5ec54192c8430de39d78153b2151022
HagenGaryP <hagengaryp@gmail.com>
2020-05-01 16:07:36 -0400

    testing

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -8,21 +8,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <h1>Casino Royal</h1>
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+
     </nav>
     <hr />
   </div>
