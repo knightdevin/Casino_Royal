29dc84d2432a8485cdc38519ac8a1dbb4e533509
Devin Knight <knightdevin@hotmail.com>
2020-05-01 15:56:45 -0400

    first commit with title update

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Casino Royal</h1>
     <nav>
       {isLoggedIn ? (
         <div>
